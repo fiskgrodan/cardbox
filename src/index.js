1587d935f046e90bab1eba33a7b7d70b15e03e39
johan <fiskgrodan@hotmail.com>
2019-04-14 14:12:51 +0200

    feat: added options and functions for autosave, save and formatting.

--- src/index.js
+++ src/index.js
@@ -6,26 +6,31 @@ const { create_random_id } = require('./utils/random.js');
 const default_options = {
 	path: './data/',
 	spaces: 2, // TODO: null by default?
-	random: () => create_random_id(5), // TODO: use 16 by default
+	random: () => create_random_id(5), // TODO: use 16 or 12 by default?
 	cache: true, // TODO: use this
+	autosave: true,
 	sync: {
 		create: false,
-		read: false,
-		update: false,
-		delete: false
+		read: true,
+		update: true,
+		delete: true
+	},
+	format: {
+		write: null,
+		open: null
 	}
 };
 
 // CardBox
 class CardBox {
-	// Init
 	constructor(options = default_options) {
 		// Options
 		this.options = Object.assign(
 			{},
 			default_options,
 			options,
-			{ sync: Object.assign({}, default_options.sync, options.sync) }
+			{ sync: Object.assign({}, default_options.sync, options.sync) },
+			{ format: Object.assign({}, default_options.format, options.format) }
 		);
 
 		// Path
@@ -34,13 +39,16 @@ class CardBox {
 		// Cards
 		this.cards = {};
 
-		console.log(this.options);
+		// Changed
+		this.changed = [];
 	};
 
+	// Helpers
 	card_path(path, card_id) {
 		return `${path}${card_id}.json`;
 	}
 
+	// Init
 	async init() {
 		return new Promise(resolve => {
 			fs.ensureDir(this.options.path, error => {
@@ -50,6 +58,7 @@ class CardBox {
 		})
 	};
 
+	// Load
 	async load() {
 		await this.init();
 
@@ -58,14 +67,38 @@ class CardBox {
 				if (error) console.error(error);
 
 				files.forEach(filePath => {
-					const card = fs.readJsonSync(filePath);
+					const card = fs.readJsonSync(filePath, error => {
+						if (error) console.error(error);
+						resolve(false);
+					});
 					this.cards[card.id] = card;
 				});
 
-				resolve();
+				resolve(true);
 			});
 		});
 	};
+
+	// Save
+	async save() {
+		return new Promise(resolve => {
+			this.changed.forEach(card_id => {
+				if (this.cards[card_id]) {
+					fs.writeJsonSync(
+						new_card_path,
+						new_card,
+						{ spaces: this.options.spaces },
+						error => {
+							if (error) console.error(error);
+							resolve(false);
+						});
+				}
+			})
+
+			this.changed = [];
+			resolve(true);
+		});
+	}
 
 	// Create
 	async create(
@@ -84,6 +117,8 @@ class CardBox {
 				{ spaces: this.options.spaces },
 				error => {
 					if (error) console.error(error);
+
+					// TODO: create sync resolve here
 				});
 
 			this.cards[new_card.id] = new_card;
@@ -94,9 +129,12 @@ class CardBox {
 	// Read
 	async read(card_id) {
 		return new Promise(resolve => {
+			// Read all cards
 			if (!card_id) {
 				resolve(Object.values(this.cards));
 			}
+
+			// Read a specific card
 			resolve(this.cards[card_id]);
 		})
 	}
@@ -120,6 +158,8 @@ class CardBox {
 
 			fs.remove(card_path, error => {
 				if (error) console.error(error);
+
+				// TODO: delete sync resolve here
 			});
 
 			resolve(delete this.cards[card_id]);
