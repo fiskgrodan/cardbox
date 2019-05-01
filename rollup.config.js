b9545120b48d261fbc6cb5bf776a4108691db7c0
johan <fiskgrodan@hotmail.com>
2019-05-01 20:01:07 +0200

    style: update rollup config format

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 export default [
 	{
-		input: 'src/main.js',
+		input: "src/main.js",
 		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
+			{ file: pkg.main, format: "cjs" },
+			{ file: pkg.module, format: "es" }
 		],
 		plugins: [
 			resolve(),
